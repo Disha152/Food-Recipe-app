0eeef061b9c76ccd7a81e88cdfed628dab3c1820
Disha152 <disha29082001@gmail.com>
2022-08-03 15:34:31 +0530

    done till adding favourites

--- app/src/main/java/com/example/foodproject/MainScreen.java
+++ app/src/main/java/com/example/foodproject/MainScreen.java
@@ -2,13 +2,26 @@ package com.example.foodproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainScreen extends AppCompatActivity {
-
+    ImageView heart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
-        getSupportActionBar().setTitle("Welcome");
+
+        getSupportActionBar().hide();
+        heart=findViewById(R.id.fav);
+        heart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainScreen.this, "Added to favourites!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
